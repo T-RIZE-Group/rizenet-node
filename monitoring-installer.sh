c5190b3fbcab789bae02584e66a5b476782f4c76
Eduardo Furtado <eduardoxfurtado@gmail.com>
2025-03-17 16:50:21 -0400

    hotfix

--- monitoring-installer.sh
+++ monitoring-installer.sh
@@ -148,7 +148,6 @@ install_grafana() {
   sudo mkdir -p /etc/apt/keyrings/
   wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/grafana.gpg > /dev/null
   echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee -a /etc/apt/sources.list.d/grafana.list
-  apt-get update -y
   apt-get install grafana -y
 
 
