7ca4b2b6bc56d9dea17f548ac3bfd6ab220f9845
Eduardo Furtado <eduardoxfurtado@gmail.com>
2025-04-06 21:23:48 -0400

    appending instead of creating a new log file every time (ops)

--- shareLogs.sh
+++ shareLogs.sh
@@ -8,11 +8,11 @@ LOG_FILE_PATH="/tmp/shareLogs-$LOG_FILE_NAME"
 
 # Get the rizenet-node directory to work with
 export SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}" 2>/dev/null || realpath "$0" 2>/dev/null)")
-echo "SCRIPT_DIR: $SCRIPT_DIR" > $LOG_FILE_PATH
+echo "SCRIPT_DIR: $SCRIPT_DIR" >> $LOG_FILE_PATH
 
 # Load util functions (like upload_encrypted_data) to encrypt files and upload metadata
-echo "Sourcing common functions from $SCRIPT_DIR/util.sh" > $LOG_FILE_PATH
-source "$SCRIPT_DIR/util.sh" > $LOG_FILE_PATH
+echo "Sourcing common functions from $SCRIPT_DIR/util.sh" >> $LOG_FILE_PATH
+source "$SCRIPT_DIR/util.sh" >> $LOG_FILE_PATH 2>&1
 
 
 
@@ -33,47 +33,37 @@ print_config_vars() {
 
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Node config:\n\n" > $LOG_FILE_PATH
-print_config_vars > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Node config:\n\n" >> $LOG_FILE_PATH
+print_config_vars >> $LOG_FILE_PATH
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Current migration ID:" > $LOG_FILE_PATH
-cat "$SCRIPT_DIR/migration" > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Current migration ID:" >> $LOG_FILE_PATH
+cat "$SCRIPT_DIR/migration" >> $LOG_FILE_PATH 2>&1
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Status of avalanchego: \n\n" > $LOG_FILE_PATH
-systemctl status avalanchego --no-pager > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Status of avalanchego: \n\n" >> $LOG_FILE_PATH
+systemctl status avalanchego --no-pager >> $LOG_FILE_PATH 2>&1
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Status of prometheus: \n\n" > $LOG_FILE_PATH
-systemctl status prometheus --no-pager > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Status of prometheus: \n\n" >> $LOG_FILE_PATH
+systemctl status prometheus --no-pager >> $LOG_FILE_PATH 2>&1
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Status of prometheus: \n\n" > $LOG_FILE_PATH
-systemctl status prometheus --no-pager > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Status of node_exporter: \n\n" >> $LOG_FILE_PATH
+systemctl status node_exporter --no-pager >> $LOG_FILE_PATH 2>&1
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Status of prometheus: \n\n" > $LOG_FILE_PATH
-systemctl status prometheus --no-pager > $LOG_FILE_PATH
+echo "\n\n" >> $LOG_FILE_PATH
+echo "Logs of avalanchego: \n\n" >> $LOG_FILE_PATH
+journalctl -u avalanchego >> $LOG_FILE_PATH 2>&1
 
 
-echo "\n\n" > $LOG_FILE_PATH
-echo "Status of node_exporter: \n\n" > $LOG_FILE_PATH
-systemctl status node_exporter --no-pager > $LOG_FILE_PATH
-
-
-echo "\n\n" > $LOG_FILE_PATH
-echo "Logs of avalanchego: \n\n" > $LOG_FILE_PATH
-journalctl -u avalanchego > $LOG_FILE_PATH
-
-
-printf '\n%.0s' {1..50} > $LOG_FILE_PATH
+printf '\n%.0s' {1..50} >> $LOG_FILE_PATH
 
 
 # generate a random encryption and decryption passphrase
@@ -83,7 +73,7 @@ passphrase=$(openssl rand -base64 16)
 encrypted_data=$(encrypt_and_output $LOG_FILE_PATH $passphrase)
 
 # Upload the encrypted data and print the output on the screen
-upload_encrypted_data "$encrypted_data" "$LOG_FILE_NAME" "$LOG_FILE_PATH" "$passphrase"
+upload_encrypted_data "$encrypted_data" "$LOG_FILE_NAME" "$LOG_FILE_PATH" "$passphrase" >> $LOG_FILE_PATH 2>&1
 
 # the script will have created two exact files. Lets delete one of them:
 rm $LOG_FILE_PATH
