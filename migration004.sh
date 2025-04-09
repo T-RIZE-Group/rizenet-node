25cd80feb954f5fc9ec8a6522f41dcd9da3a6a50
HelmiTrabelsi <hilmi-102@hotmail.com>
2025-04-09 12:28:51 -0400

    fix sudo issue and add variable to config

--- migration004.sh
+++ migration004.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # do NOT execute this script directly. Instead, run executeMigrations.sh
 
@@ -10,22 +11,39 @@
 # Changing the listen address in the node exporter service file #
 #################################################################
 
-# Define the service file path
-DEFAULT_SERVICE_FILE="/etc/systemd/system/node_exporter.service"
-# Define the default port for json exporter
-DEFAULT_JSON_EXPORTER_PORT=7979
+# Source the myNodeConfig.sh file from the same directory
+echo "loading myNodeConfig.sh"
+SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}" 2>/dev/null || realpath "$0" 2>/dev/null)")
+echo "Sourcing config from $SCRIPT_DIR/myNodeConfig.sh"
+source "$SCRIPT_DIR/myNodeConfig.sh"
 
-# Define the default port for node exporter
-DEFAULT_NODE_EXPORTER_PORT=9100
-
-# Ask for node exporter service file path
-read -rp "📄 Path to node exporter service (default: $DEFAULT_SERVICE_FILE): " SERVICE_FILE
-SERVICE_FILE=${SERVICE_FILE:-$DEFAULT_SERVICE_FILE}
-
-if [[ ! -f "$SERVICE_FILE" ]]; then
-  echo "❌ File not found: $SERVICE_FILE"
-  exit 1
+# Check if myNodeConfig.sh already contains a line with DEFAULT_SERVICE_FILE=
+if ! grep -q '^export DEFAULT_SERVICE_FILE=' "$SCRIPT_DIR/myNodeConfig.sh"; then
+  # Append the new value line if not found
+  echo '' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo '# node monitoring:' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo 'Adding DEFAULT_SERVICE_FILE="/etc/systemd/system/node_exporter.service" to $SCRIPT_DIR/myNodeConfig.sh'
+  echo 'export DEFAULT_SERVICE_FILE="/etc/systemd/system/node_exporter.service"' >> "$SCRIPT_DIR/myNodeConfig.sh"
 fi
+
+# Check if myNodeConfig.sh already contains a line with DEFAULT_JSON_EXPORTER_PORT=
+if ! grep -q '^export DEFAULT_JSON_EXPORTER_PORT=' "$SCRIPT_DIR/myNodeConfig.sh"; then
+  # Append the new value line if not found
+  echo '' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo '# node monitoring:' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo 'Adding DEFAULT_JSON_EXPORTER_PORT=7979 to $SCRIPT_DIR/myNodeConfig.sh'
+  echo 'export DEFAULT_JSON_EXPORTER_PORT=7979' >> "$SCRIPT_DIR/myNodeConfig.sh"
+fi
+
+# Check if myNodeConfig.sh already contains a line with DEFAULT_NODE_EXPORTER_PORT=
+if ! grep -q '^export DEFAULT_NODE_EXPORTER_PORT=' "$SCRIPT_DIR/myNodeConfig.sh"; then
+  # Append the new value line if not found
+  echo '' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo '# node monitoring:' >> "$SCRIPT_DIR/myNodeConfig.sh"
+  echo 'Adding DEFAULT_NODE_EXPORTER_PORT=9100 to $SCRIPT_DIR/myNodeConfig.sh'
+  echo 'export DEFAULT_NODE_EXPORTER_PORT=9100' >> "$SCRIPT_DIR/myNodeConfig.sh"
+fi
+
 
 # Check if the file exists
 if [ ! -f "$SERVICE_FILE" ]; then
@@ -33,22 +51,16 @@ if [ ! -f "$SERVICE_FILE" ]; then
   exit 1
 fi
 
-# Prompt the user for the Json Exporter port number
-read -p "Enter the json exporter port number (default is $DEFAULT_JSON_EXPORTER_PORT): " DEFAULT_JSON_EXPORTER_PORT
-JSON_EXPORTER_PORT=${JSON_EXPORTER_PORT:-$DEFAULT_JSON_EXPORTER_PORT}
 
-# Prompt the user for the Node Exporter port number
-read -p "Enter the node exporter port number (default is $DEFAULT_NODE_EXPORTER_PORT): " DEFAULT_NODE_EXPORTER_PORT
-NODE_EXPORTER_PORT=${NODE_EXPORTER_PORT:-$DEFAULT_NODE_EXPORTER_PORT}
 
 LISTEN_IP='0.0.0.0'
 
 # Replace the listen address
-sudo sed -i "s|.*--web.listen-address=.*|    --web.listen-address=${LISTEN_IP}:${NODE_EXPORTER_PORT} \\\\|" "$SERVICE_FILE"
+sed -i "s|.*--web.listen-address=.*|    --web.listen-address=${LISTEN_IP}:${NODE_EXPORTER_PORT} \\\\|" "$SERVICE_FILE"
 
 # Reload systemd and restart node_exporter
-sudo systemctl daemon-reload
-sudo systemctl restart node_exporter
+systemctl daemon-reload
+systemctl restart node_exporter
 
 echo "✅ node_exporter service updated and restarted"
 
@@ -58,10 +70,15 @@ echo "✅ node_exporter service updated and restarted"
 #################################################################
 
 # Step 1: Clone the json_exporter repository
+JSON_EXPORTER_HOME="/opt/json_exporter"
+
 echo "Checking if json_exporter repository exists..."
 if [ ! -d "json_exporter" ]; then
-  echo "Cloning json_exporter repository..."
-  git clone https://github.com/prometheus-community/json_exporter.git
+    echo "Cloning json_exporter repository..."
+    mkdir -p /opt/json_exporter
+    chown $USER:$USER /opt/json_exporter
+    cd /opt/json_exporter
+    sudo -u "$USER_NAME" bash -lc "git clone https://github.com/prometheus-community/json_exporter.git ."
 else
   echo "json_exporter repository already exists. Skipping clone."
 fi
@@ -70,12 +87,13 @@ cd json_exporter/
 
 # Step 2: Install necessary dependencies
 echo "Installing make..."
-sudo apt update
-sudo apt install -y make
+apt update
+apt install -y make
 
 # Step 3: Build the json_exporter
+echo 'export PATH=$PATH:/usr/local/go/bin' | tee -a /home/${USER_NAME}/.profile
 echo "Building json_exporter..."
-make build
+sudo -u "$USER_NAME" bash -lc "make build"
 
 # Step 4: Modify the config.yml
 echo "Modifying config.yml..."
@@ -89,20 +107,18 @@ modules:
       path: '{ .healthy }'
       help: healthy is true all the health checks are passing
 EOL
-fi
 
 # Step 5: Create the systemd service for json_exporter
 echo "Creating systemd service for json_exporter..."
-JSON_EXPORTER_HOME=$(sudo find / -type d -name "json_exporter" 2>/dev/null)
 
-sudo tee /etc/systemd/system/json_exporter.service > /dev/null <<EOL
+tee /etc/systemd/system/json_exporter.service > /dev/null <<EOL
 [Unit]
 Description=Prometheus JSON Exporter
 After=network.target
 
 [Service]
 Type=simple
-User=${USER}
+User=${USER_NAME}
 WorkingDirectory=${JSON_EXPORTER_HOME}
 ExecStart=${JSON_EXPORTER_HOME}/json_exporter \
   --config.file ${JSON_EXPORTER_HOME}/examples/config.yml \
@@ -116,9 +132,9 @@ EOL
 
 # Step 6: Reload systemd, restart, enable and check the service
 echo "Reloading systemd and restarting json_exporter..."
-sudo systemctl daemon-reload
-sudo systemctl restart json_exporter
-sudo systemctl enable json_exporter
+systemctl daemon-reload
+systemctl restart json_exporter
+systemctl enable json_exporter
 
 echo "✅ json_exporter setup complete!"
 
