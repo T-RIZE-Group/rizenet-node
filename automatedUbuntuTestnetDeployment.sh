4e27041c74f3a528966210871df221461eed8e75
Eduardo Furtado <eduardoxfurtado@gmail.com>
2025-04-14 14:46:35 -0400

    disabling IPv6 on new nodes if they are using a dynamic IP

--- automatedUbuntuTestnetDeployment.sh
+++ automatedUbuntuTestnetDeployment.sh
@@ -186,6 +186,31 @@ if [ "$HAS_DYNAMIC_IP" = "true" ]; then
   # Change the public IP update frequency from the default of 5 minutes to 1 minute:
   publicIpResolutionFrequency='"public-ip-resolution-frequency": "1m0s",'
 
+  # both ifconfigCo and opendns can return an IPv6 address, which is not supported by Avalanche.
+  # for this reason, we must disable IPv6 on nodes with dynamic IP:
+  # Function: update or add a sysctl setting
+  update_sysctl_setting() {
+      local setting="$1"  # e.g., net.ipv6.conf.all.disable_ipv6
+      local value="$2"    # e.g., 1
+      local line="${setting} = ${value}"
+
+      # Check if the setting already exists
+      if grep -q "^${setting}" "/etc/sysctl.conf"; then
+          # Replace the line with the correct value if it's different
+          sudo sed -i "s|^${setting}.*|${line}|g" "/etc/sysctl.conf"
+      else
+          # Append the setting if not found
+          echo "${line}" | sudo tee -a "/etc/sysctl.conf" > /dev/null
+      fi
+  }
+
+  # Update the IPv6 disabling settings
+  update_sysctl_setting "net.ipv6.conf.all.disable_ipv6" "1"
+  update_sysctl_setting "net.ipv6.conf.default.disable_ipv6" "1"
+
+  # Reload sysctl settings
+  sudo sysctl -p
+
 else
   # get the public external IP, forcing it to be IPv4 with `-4`:
   publicIp='"public-ip": "'$(curl -s -4 ifconfig.co | tr -d '[:space:]')'",'
